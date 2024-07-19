6db2da65481b2f2dc27fc815404641d994d9c1b9
Dhineshdk <dinesh.k131997@gmail.com>
2024-07-20 03:22:01 +0530

    Add article detail screen

--- app/src/main/java/com/example/learntogether/MainActivity.kt
+++ app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.example.learntogether
 
+import androidx.compose.foundation.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.learntogether.ui.theme.LearnTogetherTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +28,7 @@ class MainActivity : ComponentActivity() {
             LearnTogetherTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    ArticleDetail()
                 }
             }
         }
@@ -27,17 +36,43 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ArticleDetail() {
+    Article(
+        title = stringResource(R.string.articleTitle),
+        introduction =stringResource(R.string.articleIntroduction) ,
+        body =stringResource(R.string.articleBody),
+        image = painterResource(R.drawable.article_image),
+}
+
+@Composable
+fun Article(title: String,introduction: String,body: String,image: Painter) {
+    Column(){
+        Image(
+            painter =image,
+            contentDescription = "Jackpot Compose Button"
+        )
+        Text(
+            text= title,
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = introduction,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = body,
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ArticlePreview() {
     LearnTogetherTheme {
-        Greeting("Android")
+        ArticleDetail()
     }
 }
